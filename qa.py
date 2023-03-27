49cc626a6ef954b4af16535bfda9cccd36194201
Tony Pai <tony77794@gmail.com>
2023-03-28 01:27:58 +0800

    Integrate telegram bot and remove flask

--- qa.py
+++ qa.py
@@ -2,6 +2,13 @@
 # coding: utf-8
 
 # Import necessary libraries
+import os
+os.environ['OPENAI_API_KEY'] = '...'
+os.environ['TELEGRAM_TOKEN'] = '...'
+
+import telegram
+from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBuilder
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
@@ -9,13 +16,19 @@ from langchain import OpenAI, VectorDBQA, PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.prompts.chat import HumanMessagePromptTemplate
-from flask import Flask, request, jsonify
-import os
-# os.environ['OPENAI_API_KEY'] = '...'
 
-app = Flask(__name__)
+def init_tg_bot():
+    try:
+        application = ApplicationBuilder().token(os.environ['TELEGRAM_TOKEN']).build()
+        start_handler = CommandHandler('start', start)
+        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+        application.add_handler(start_handler)
+        application.add_handler(echo_handler)
+        application.run_polling()
+    except Exception as e:
+        print(e)
 
-def setup_chatbot():
+def setup_chatgpt():
     chat=ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0)
     return chat
 
@@ -35,7 +48,7 @@ def setup_embeddings():
     return embeddings
 
 def setup_vector_search(texts, embeddings):
-    docsearch = Chroma.from_documents(texts, embeddings, persist_directory="./chroma-data")
+    docsearch = Chroma(embedding_function=embeddings, persist_directory="./chroma-data")
     return docsearch
 
 def setup_qa_model(docsearch):
@@ -55,7 +68,7 @@ def ask_chatbot(human_message, chat):
     return q
 
 def get_answer(q, qa):
-    a=qa.run(q.content)
+    a=qa.run(q.content + ' Please provide the relevant legal provisions and the name of the law. Please provide a brief and straightforward answer, avoiding the use of double negatives.')
     print('a=' + a)
     return a
 
@@ -71,7 +84,23 @@ def translate_answer_to_chinese(a, chat, human_message_prompt):
     output=chat(human_message)
     return output.content
 
-chat = setup_chatbot()
+async def start(update, context):
+    try:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="此機器人僅供問答而非聊天，因此不能連續發問，每個問題都是獨立的。目前僅載入勞動基準法，你可以嘗試提問：「我老闆沒通知就解僱我，這合法嗎？」")
+    except Exception as e:
+        print(e)
+
+async def echo(update, context):
+    try:
+        human_message=human_message_prompt.format_messages(text=update.message.text)
+        q = ask_chatbot(human_message, chat)
+        a = get_answer(q, qa)
+        output = translate_answer_to_chinese(a, chat, human_message_prompt_2)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=output)
+    except Exception as e:
+        print(e)
+
+chat = setup_chatgpt()
 docs = load_data()
 texts = split_documents(docs)
 embeddings = setup_embeddings()
@@ -79,18 +108,6 @@ docsearch = setup_vector_search(texts, embeddings)
 qa = setup_qa_model(docsearch)
 human_message_prompt = setup_human_message_prompt()
 human_message_prompt_2 = setup_human_message_prompt_2()
-
-@app.route('/ask', methods=['POST'])
-def ask_api():
-    human_message=human_message_prompt.format_messages(text=request.json['input'])
-    q = ask_chatbot(human_message, chat)
-    a = get_answer(q, qa)
-    output = translate_answer_to_chinese(a, chat, human_message_prompt_2)
-    response = jsonify({'response': output})
-    response.headers.add('Access-Control-Allow-Origin', os.environ['HOSTNAME'])
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'POST,OPTIONS')
-    return response
-
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+        
+# Initialize Telegram bot
+init_tg_bot()
