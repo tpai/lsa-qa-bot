31092cf70af42929dcb6d331827a7c64dbe2ead3
Tony Pai <tony77794@gmail.com>
2023-03-31 00:19:18 +0800

    Refactor code to support Telegram bot and add "law" command handler for querying legal provisions

--- qa.py
+++ qa.py
@@ -11,36 +11,25 @@ from telegram.ext import CommandHandler, MessageHandler, filters, ApplicationBui
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
-from langchain import OpenAI, PromptTemplate
+from langchain.text_splitter import TokenTextSplitter
+from langchain import OpenAI
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.prompts.chat import HumanMessagePromptTemplate
+from langchain.document_loaders import DirectoryLoader, UnstructuredFileLoader
 from langchain.chains import RetrievalQA
-
-def init_tg_bot():
-    try:
-        application = ApplicationBuilder().token(os.environ['TELEGRAM_TOKEN']).build()
-        start_handler = CommandHandler('start', start)
-        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-        application.add_handler(start_handler)
-        application.add_handler(echo_handler)
-        application.run_polling()
-    except Exception as e:
-        print(e)
 
 def setup_chatgpt():
     chat=ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0)
     return chat
 
-def load_data():
-    loader = UnstructuredFileLoader('./data/labor_standard_act.txt')
+def load_file():
+    loader = UnstructuredFileLoader('./files/cut/labor_standard_act_zhtw.txt')
+    # loader = DirectoryLoader('./files/cut/', glob='**/*.txt')
     docs = loader.load()
-    print('data loaded')
+    print('file loaded')
     return docs
 
 def split_documents(docs):
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = TokenTextSplitter(chunk_size=500, chunk_overlap=100)
     texts = text_splitter.split_documents(docs)
     return texts
 
@@ -50,65 +39,74 @@ def setup_embeddings():
 
 def setup_vector_search(texts, embeddings):
     docsearch = Chroma(embedding_function=embeddings, persist_directory="./chroma-data")
+    # docsearch = Chroma.from_documents(texts, embeddings, persist_directory="./chroma-data")
+    docsearch.persist()
     return docsearch
 
 def setup_qa_model(docsearch):
     qa = RetrievalQA.from_llm(llm=OpenAI(), retriever=docsearch.as_retriever())
     return qa
 
-def setup_human_message_prompt():
-    human_message_prompt=HumanMessagePromptTemplate(prompt=PromptTemplate(
-        template="Translate the following input to English: {text}",
-        input_variables=["text"],
-    ))
-    return human_message_prompt
-
-def ask_chatbot(human_message, chat):
-    q=chat(human_message)
-    print('q=' + q.content)
-    return q
-
-def get_answer(q, qa):
-    a=qa.run(q.content + ' Please provide the relevant legal provisions and the name of the law. Please provide a brief and straightforward answer, avoiding the use of double negatives.')
-    print('a=' + a)
+def get_answer(q, qa, prefix='', suffix=''):
+    q_map = {'勞基法': '勞動基準法', '老闆': '雇主', '員工': '勞工', '薪水': '工資', '工時': '工作時間', '特休': '特別休假', '未成年': '童工', '工讀生': '技術生', '年終': '年度終了', '勞退': '勞工退休金', '懷孕': '妊娠'}
+    for key, value in q_map.items():
+        q = q.replace(key, value)
+    q_fixed = f"{prefix}{q}{suffix}"
+    print(f'q={q_fixed}')
+    a = qa.run(q_fixed)
+    print(f'a={a}')
     return a
-
-def setup_human_message_prompt_2():
-    human_message_prompt=HumanMessagePromptTemplate(prompt=PromptTemplate(
-        template="Translate the following input to Traditional Chinese: {text}",
-        input_variables=["text"],
-    ))
-    return human_message_prompt
-
-def translate_answer_to_chinese(a, chat, human_message_prompt):
-    human_message=human_message_prompt.format_messages(text=a)
-    output=chat(human_message)
-    return output.content
 
 async def start(update, context):
     try:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="此機器人僅供問答而非聊天，因此不能連續發問，每個問題都是獨立的。目前僅載入勞動基準法，你可以嘗試提問：「我老闆沒通知就解僱我，這合法嗎？」")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="此機器人僅供問答而非聊天，因此不能連續發問，每個問題都是獨立的。\n你可以嘗試問「老闆欠薪」或者試試附上法條的「/law 老闆欠薪」。")
     except Exception as e:
+        print(e)
+
+async def law(update, context):
+    try:
+        a = get_answer(update.message.text.split(' ', 1)[1], qa, '', ' 請簡單回答，並附上相關的法條（如：第X條第X項）和情事。')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=a)
+    except Exception as e:
+        if 'index out of range' in str(e):
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="請輸入你想問的問題")
+            return
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="不明錯誤")
         print(e)
 
 async def echo(update, context):
     try:
-        human_message=human_message_prompt.format_messages(text=update.message.text)
-        q = ask_chatbot(human_message, chat)
-        a = get_answer(q, qa)
-        output = translate_answer_to_chinese(a, chat, human_message_prompt_2)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=output)
+        a = get_answer(update.message.text, qa, '', ' 請簡單回答。')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=a)
     except Exception as e:
+        if 'reduce your prompt' in str(e):
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="請精簡你的問題")
+            return
+        if 'exceeded your current quota' in str(e):
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="API Quota 用完囉")
+            return
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="不明錯誤")
         print(e)
 
+def init_tg_bot():
+    try:
+        application = ApplicationBuilder().token(os.environ['TELEGRAM_TOKEN']).build()
+        start_handler = CommandHandler('start', start)
+        law_handler = CommandHandler('law', law)
+        echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+        application.add_handler(start_handler)
+        application.add_handler(law_handler)
+        application.add_handler(echo_handler)
+        application.run_polling()
+    except Exception as e:
+        print(e)
+        
 chat = setup_chatgpt()
-docs = load_data()
+docs = load_file()
 texts = split_documents(docs)
 embeddings = setup_embeddings()
 docsearch = setup_vector_search(texts, embeddings)
 qa = setup_qa_model(docsearch)
-human_message_prompt = setup_human_message_prompt()
-human_message_prompt_2 = setup_human_message_prompt_2()
-        
+
 # Initialize Telegram bot
 init_tg_bot()
